54f3bcd14c8be713d17df82c32912e1e6080d1af
Yu2erer <yu2erer@gmail.com>
2020-02-03 10:31:48 +0800

    FIX BUG: Mac OSX Retina Half-Size.

--- src/main/java/LZSB/avada/Main.java
+++ src/main/java/LZSB/avada/Main.java
@@ -9,11 +9,14 @@ import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 import com.jme3.system.AppSettings;
-
+import com.jme3.util.BufferUtils;
+import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.glfw.GLFW;
+import com.jme3.system.lwjgl.LwjglWindow;
 import java.awt.*;
+import java.nio.IntBuffer;
 
 public class Main extends SimpleApplication {
-
 
     public static void main(String[] args) {
 
@@ -23,26 +26,32 @@ public class Main extends SimpleApplication {
 
         AppSettings settings = new AppSettings(true);
         settings.setTitle("My Awesome Game");
+
         settings.setResolution(1024,768);
         app.setSettings(settings);
-
         app.start();
-
     }
     private Spatial model;
     @Override
     public void simpleInitApp() {
+        // FIX BUG: Mac OSX Retina Half-Size.
+        IntBuffer framebufferSize = BufferUtils.createIntBuffer(2);
+        long window = ((LwjglWindow)getContext()).getWindowHandle();
+        GLFW.nglfwGetFramebufferSize(window, MemoryUtil.memAddress(framebufferSize), MemoryUtil.memAddress(framebufferSize) + 4);
+        int width = framebufferSize.get(0);
+        int height = framebufferSize.get(1);
+        reshape(width, height);
 
         cam.setLocation(new Vector3f(0.41600543f, 3.2057908f, 6.6927643f));
         cam.setRotation(new Quaternion(-0.00414816f, 0.9817784f, -0.18875499f, -0.021575727f));
         flyCam.setMoveSpeed(10);
         viewPort.setBackgroundColor(ColorRGBA.LightGray);
-        reshape(1024,768);
 
 
         model = assetManager.loadModel("Models/arc/ArcDeTriomphe.obj");
         model.scale(0.05f);
         model.center();
+
 
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-1,-2,-3));

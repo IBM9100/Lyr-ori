6a616c74aba8234e73ad841315f2ded76b4e3d36
IBM9100 <chuyuekayo@gmail.com>
2020-02-05 13:27:05 +0800

    [+] 实现随机地图（暂定）

--- src/main/java/LZSB/avada/Main.java
+++ src/main/java/LZSB/avada/Main.java
@@ -1,5 +1,6 @@
 package LZSB.avada;
 
+import LZSB.avada.Cameras.MainCamAppState;
 import LZSB.avada.Compatibility.NormalizationAppState;
 import LZSB.avada.Lumos.LightAppState;
 import LZSB.avada.Lumos.SkyAppState;
@@ -15,6 +16,11 @@ import com.jme3.system.AppSettings;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.system.CallbackI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 
 public class Main extends SimpleApplication {
@@ -37,11 +43,25 @@ public class Main extends SimpleApplication {
         app.start();
     }
 
+    int checkAroundCount(boolean[][] pMap,int range,int x,int y){
+        int count = 0;
+        for(int i = -range;i <= range;i++){
+            for(int j = -range;j <= range;j++){
+                if(x+i < 0 || y+j < 0 ||(x+i == 0 && y+j == 0) || x+i == 50 || y+j == 50){
+                    continue;
+                }
+                if (pMap[x+i][y+j]){
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
 
     private Spatial model;
     @Override
     public void simpleInitApp() {
-        stateManager.attachAll(new NormalizationAppState(),new SkyAppState(),new LightAppState());
+        stateManager.attachAll(new NormalizationAppState(),new SkyAppState(),new LightAppState(),new MainCamAppState());
 
         viewPort.setBackgroundColor(ColorRGBA.LightGray);
 
@@ -49,20 +69,68 @@ public class Main extends SimpleApplication {
         fpp.setNumSamples(4);
         viewPort.addProcessor(fpp);
 
+        String seed = "LZSB";
+        Random random = new Random(seed.hashCode());
+
+        long startTime = System.currentTimeMillis();
+
+        boolean[][] map = new boolean[50][50];
+        for (int i = 0; i < map.length;i++){
+            for (int j = 0;j < map[i].length; j++){
+                map[i][j] = random.nextBoolean();
+            }
+        }
+        //times应该为偶数，以保证map得到正确的引用
+        for (int times = 0; times <4; times++){
+            boolean[][] frozenMap = new boolean[50][50];
+            for (int i = 0; i < map.length;i++){
+
+                for (int j = 0;j < map[i].length; j++){
+                    if (map[i][j]){
+                        frozenMap[i][j] = checkAroundCount(map, 1, i, j) >= 4;
+                    }else {
+                        frozenMap[i][j] = checkAroundCount(map, 1, i, j) >= 5;
+                    }
+                }
+            }
+            boolean[][] changer = frozenMap.clone();
+            frozenMap = map.clone(); map = changer.clone();
+        }
         Node block =  new Node();
         model = assetManager.loadModel("Models/block/grass_block/grass_block.obj");
         model.scale(1f);
         model.center();
-        block.attachChild(model);
+        BoundingBox modelBox = (BoundingBox) model.getWorldBound();
+
+        float offsetX = 0;
+        for (int i = 0; i < map.length;i++){
+            for (int j = 0;j < map[i].length; j++){
+               if (map[i][j]){
+                   block.attachChild(model.clone());
+               }
+               model.move(new Vector3f(2*modelBox.getXExtent(),0f,0f));
+               offsetX +=2*modelBox.getXExtent();
+            }
+            model.move(new Vector3f(-offsetX,0f,0f));
+            model.move(new Vector3f(0f,2*modelBox.getYExtent(),0f));
+            offsetX = 0;
+        }
+        long endTime = System.currentTimeMillis();
+
+        log.info("运行时间："+ (endTime - startTime) +"ms");
+
+
+
+
 
 
         //为了拿到 model 的尺寸
-        BoundingBox modelBox = (BoundingBox) model.getWorldBound();
-        for (int i = 0;i<24;i++){
-            Spatial cloneSpatial = model.clone();
-            cloneSpatial.move(new Vector3f((i+1) * 2 *modelBox.getXExtent(),0f,0f));
-            block.attachChild(cloneSpatial);
-        }
+
+//        for (int i = 0;i<24;i++){
+//            Spatial cloneSpatial = model.clone();
+//            cloneSpatial.move(new Vector3f(,0f,0f));
+//            block.attachChild(cloneSpatial);
+//        }
 
 
 
